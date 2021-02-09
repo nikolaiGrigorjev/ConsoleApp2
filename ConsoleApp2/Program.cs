8848f760611f020f98eab3fec3dfc42ce59194c1
Opilane TTHK <opilane@tthk.ee>
2021-02-09 12:25:33 +0200

    Add project files.

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Write a:");
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Write b:");
+            int b = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Write z:");
+            int z = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Write x:");
+            int x = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine($"")
+            
+        }
+    }
+}
